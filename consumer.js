d45801043a72107dc9dfd4be94e3521be1b106f1
Pariwat <pariwat@theiconweb.com>
2019-06-27 19:59:12 +0700

    FiX

--- consumer.js
+++ consumer.js
@@ -13,9 +13,16 @@ const topics = [
 const options = {
     autoCommit: false
 };
- 
-const consumer = new kafka.Consumer(client, topics, options);
- 
-consumer.on("message", function(message) { 
-    console.log(message)
-});
+
+const consumers = () => {
+    const consumer = new kafka.Consumer(client, topics, options);
+    
+    consumer.on("message", function(message) { 
+        console.log(message)
+    });
+
+};
+
+setTimeout(() => {
+    consumers();
+}, 1000);
