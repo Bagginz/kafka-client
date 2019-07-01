7a3e9676e3f7c08b14ed19a91966812906bfcac7
Pariwat <pariwat@theiconweb.com>
2019-07-01 23:57:40 +0700

    Fix Consumer

--- consumer.js
+++ consumer.js
@@ -1,28 +1,16 @@
-const kafka =  require("kafka-node")
- 
-const client = new kafka.KafkaClient({
-	autoConnect: false,
-	kafkaHost: 'localhost:2181'
-});
- 
-const topics = [
-    {
-        topic: "kafka-node-dev"
-    }
-];
-const options = {
-    autoCommit: false
-};
+var kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
+    client = new kafka.KafkaClient(),
+    consumer = new Consumer(
+        client,
+        [
+            { topic: 'SampleTopic'}
+        ],
+        {
+            autoCommit: false
+        }
+    );
 
-exports.consumers = () => {
-    const consumer = new kafka.Consumer(client, topics, options);
-    
-    consumer.on("message", function(message) { 
-        console.log(message)
+    consumer.on('message', function (message) {
+        console.log(message);
     });
-
-};
-
-setTimeout(() => {
-    exports.consumers();
-}, 1000);
