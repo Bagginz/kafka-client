70b7694c43fa11bed2e012c1a2d73cd75a7de517
Pariwat <pariwat@theiconweb.com>
2019-07-01 23:53:21 +0700

    Fix Producer

--- producer.js
+++ producer.js
@@ -1,28 +1,15 @@
-const kafka =  require("kafka-node")
-const client = new kafka.KafkaClient({
-	autoConnect: false,
-	kafkaHost: 'localhost:2181'
-});
-
-exports.producers = () => {
-    const Producer = kafka.Producer
-    const producer = new Producer(client);
-    const sentMessage = JSON.stringify("Hello Bagginz");
-    producer.on('ready', function () {
-        console.log('Producer is ready');
-        send(sentMessage);
-    });
-};
-
-function send(sentMessage) {
+var kafka = require('kafka-node'),
+    HighLevelProducer = kafka.HighLevelProducer,
+    client = new kafka.KafkaClient(),
+    producer = new HighLevelProducer(client),
     payloads = [
-        { topic: "TutorialTopic", messages:sentMessage , partition: 0 }
+        { topic: 'SampleTopic', messages: 'hi' },
+        { topic: 'SampleTopic', messages: ['hello', 'world'] }
     ];
-    producer.send(payloads, function (err, data) {
-       console.log("send data ",sentMessage);
-    });
-}
 
-setTimeout(() => {
-    exports.producers();
-}, 5000);
+producer.on('ready', function () {
+    console.log('PRODUCER READY !!');
+    producer.send(payloads, function (err, data) {
+        console.log(data);
+    });
+});
